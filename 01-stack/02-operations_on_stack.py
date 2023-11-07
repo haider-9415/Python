37181057f8b51ea7333cf3ffb04eb4fe5d17ae15
haider-9415 <116593197+haider-9415@users.noreply.github.com>
2023-11-08 00:06:50 +0530

    Add files via upload

---
+++ 01-stack/02-operations_on_stack.py
@@ -0,0 +1,15 @@
+"""
+    1) size --> it defines the max. of elements that the stack will hold
+
+    2) top --> it refers to the end from where the elements are added or deleted
+
+    3) push --> it adds a new element at the top of the stack
+
+    4) pop --> it removes the top element of the stack
+
+    5) isFull --> to check that a stack is completely full or not
+
+    6) isEmpty --> to check that a stack is empty or not
+
+    7) peek --> to know which element is at top of the stack
+"""
---
+++ 02-queue/02-types_of_queue.py
@@ -0,0 +1,15 @@
+"""
+    it is used in several forms depending upon the requirment of the program
+
+    there are four types of queue
+    1) simple queue: it is in straight line form
+                     in this, insertion occurs at the "rear" and removal occurs at the "front"
+                     it strictly follows the FIFO order
+    
+    2) circular queue: it is in circular form 
+                       it is used to overcome the problem of unutilized spaces in fixed size linear queue
+                       it is used in that languages which allow the use of fixed size linear structure like c. c++,java, etc.
+
+    3) priority queue: in this, each element is associated with priority and is served according to its priority
+    
+"""
